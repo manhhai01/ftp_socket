20d0419eb18657ca2d5b4370b0ebdb392b200e78
truongchinh1211 <nntchinh2001@gmail.com>
2023-10-24 19:31:39 +0700

    update mainLayout

--- fpt_socket/src/main/java/view/custom/RoundPanel.java
+++ fpt_socket/src/main/java/view/custom/RoundPanel.java
@@ -7,16 +7,24 @@ import javax.swing.JPanel;
 
 public class RoundPanel extends JPanel {
 
+
     public RoundPanel() {
         setOpaque(false);
     }
+    public int getRadius() {
+        return radius;
+    }
 
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+    private int radius=20;
     @Override
     public void paint(Graphics grphcs) {
         Graphics2D g2 = (Graphics2D) grphcs.create();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2.setColor(getBackground());
-        g2.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
         g2.dispose();
         super.paint(grphcs);
     }
